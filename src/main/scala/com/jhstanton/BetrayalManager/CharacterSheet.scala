dd18c45fca5cbe1210b894b5cbfd9c1d4c241d37
Jim Stanton <jim@jhstanton.com>
2015-05-30 10:42:30 -0500

    Now able to properly handle app destruction across multiple character sheets

--- src/main/scala/com/jhstanton/BetrayalManager/CharacterSheet.scala
+++ src/main/scala/com/jhstanton/BetrayalManager/CharacterSheet.scala
@@ -1,6 +1,6 @@
 package com.jhstanton.BetrayalManager
 
-//import android.app.Activity
+import android.app.Activity
 import android.content.{Context, Intent}
 import android.os.Bundle
 //import android.util.AttributeSet
@@ -8,7 +8,7 @@ import android.view.{View, LayoutInflater}// ViewGroup, View}
 import android.view.ViewGroup.{LayoutParams}
 import android.graphics.Color
 import android.widget.LinearLayout//{Button, TextView, LinearLayout}
-//import android.support.v4.app.Fragment;
+import android.content.SharedPreferences
 
 import org.scaloid.common._
 
@@ -18,25 +18,28 @@ import org.scaloid.common._
 // public
 object CharacterSheet extends SActivity {
   def apply()     = new CharacterSheet()
-  val speed_key     = "Speed"
-  val might_key     = "Might"
-  val knowledge_key = "Knowledge"
-  val sanity_key    = "Sanity"
 }
+
 class CharacterSheet extends SActivity {
   val params = new LinearLayout.LayoutParams( LayoutParams.MATCH_PARENT
 					      , LayoutParams.MATCH_PARENT
 					      , 1.0f)
+  val packageInfo = "com.jhstanton.BetrayalManager.CharacterSheet"
   val stats = scala.collection.mutable.Map[String, Int]()
+  var name  = ""
+  
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     val center = 0x11
     val intent : Intent = getIntent
     val name : String = intent.getStringExtra(CharacterMenuActivity.EXTRA_MESSAGE)
-    // these two values are provided by CharacterSheet when it refreshes itself for + or - btn presses
+
+    this.name = name // needed to save state :/, see onBackPressed
+
     val scrollEnvironment = new SScrollView()
     val topView = new SVerticalLayout()
     scrollEnvironment += topView
+
     characters.find(_.name == name) match {
       case None => alert("Key Error", "Unable to find character: " + name)
       case Some(character) => {
@@ -44,19 +47,29 @@ class CharacterSheet extends SActivity {
         topView += new STextView("Birthday: " + character.birthday + " Age: " + character.age).gravity(center)
 	topView += new STextView(character.hobbies).gravity(center)
 	
+	// create new stats from defaults, or take a saved instance (lost focus), or take from preferences (app destroyed)
+	val prefs = getSharedPreferences(packageInfo + name, 0)
 	character.statMap.foreach { case (stat, value) => 
-	  if (savedInstanceState == null)
-	    this.stats +=((stat, value))
+	   if (savedInstanceState == null && prefs == null) 
+	    this.stats +=((stat, value)) // pull from default
+	  else if(prefs == null) 
+	    this.stats +=((stat, savedInstanceState.getInt(stat))) // pull from last saved instance
 	  else
-	    this.stats +=((stat, savedInstanceState.getInt(stat)))
+	    this.stats += ( (stat, prefs.getInt(stat, value) )) //pull from last created instance
+	  /*if (savedInstanceState == null) 
+	    this.stats +=((stat, value)) // pull from default
+	  else if(prefs == null) 
+	    this.stats +=((stat, savedInstanceState.getInt(stat))) // pull from last saved instance
+	  else
+	    this.stats += ( (stat, prefs.getInt(stat, value) )) //pull from last created instance*/
 	}	
 	  
 	val skillView = new SLinearLayout()
 	skillView.setLayoutParams(params)
-	List(character.speedStats, character.mightStats, character.knowledgeStats, character.sanityStats)
+	val resetFunctions = List(character.speedStats, character.mightStats, character.knowledgeStats, character.sanityStats)
 	  .zip(character.statMap.toList)              //List("Speed", "Might", "Knowledge", "Sanity"))
-	  .foreach{ case(stats, (statName, statIndex)) =>
-		    val statView = new SVerticalLayout() //.Weight(1.0f)
+	  .map{ case(stats, (statName, statIndex)) =>
+		    val statView = new SVerticalLayout() 
 		    statView.setLayoutParams(params)
 		    statView += new STextView(statName).gravity(center)
 		    val enumeratedStats = stats.zip(0 to 8)
@@ -86,20 +99,37 @@ class CharacterSheet extends SActivity {
 		    statView += plusButton
 		    statView += minusButton
 		    skillView += statView
+		    // returns a reset function
+		    (statName, (originalIndex: Int) => {
+		      statValViews(this.stats(statName)).setTextColor(Color.WHITE)
+		      this.stats += ( (statName, originalIndex) )
+		      statValViews(this.stats(statName)).setTextColor(Color.RED)
+		    })
 		  }
 	topView += skillView
-      }	
+	val resetBtn = new SButton("Reset", (view: View) => resetFunctions.foreach{ 
+	  case (statName, resetter) => resetter(character.statMap(statName))
+	})
+	topView += resetBtn
+     }	
     }
-    setContentView(scrollEnvironment) //contentView = scrollEnvironment
+    setContentView(scrollEnvironment) 
   }
 
   override def onSaveInstanceState(savedInstanceState: Bundle) {
     this.stats.foreach{ case (statName, statVal) => savedInstanceState.putInt(statName, statVal) }
     super.onSaveInstanceState(savedInstanceState)
   }
+
+  override def onBackPressed() {
+    val editor : SharedPreferences.Editor = getSharedPreferences(packageInfo + name, 0).edit()  // 0 specifies private mode
+    stats foreach { case (statName, value) => editor.putInt(statName, value) }
+    editor.commit
+    super.onBackPressed()
+  }
   // these should probably be moved elsewhere..
   val characters : Array[Character] = Array(
-    new Character("Heather Granville", 18, "August 2nd", "Television, Shopping", 5, 5, 5 , 3, Array(8, 7, 6, 6, 5, 4, 3, 3, 0), Array(8, 7, 6, 5, 4, 3, 3, 3, 0), Array(6, 6, 6, 5, 4, 3, 3, 3, 0), Array(8, 7, 6, 5, 4, 3, 3, 2, 0)),
+    new Character("Heather Granville", 18, "August 2nd", "Television, Shopping", 5, 5, 5, 3, Array(8, 7, 6, 6, 5, 4, 3, 3, 0), Array(8, 7, 6, 5, 4, 3, 3, 3, 0), Array(6, 6, 6, 5, 4, 3, 3, 3, 0), Array(8, 7, 6, 5, 4, 3, 3, 2, 0)),
     new Character("Jenny LeClerc", 21, "March 4th", "Reading, Soccer", 4, 5, 3, 5, Array(8, 6, 5, 4, 4, 4, 3, 2, 0), Array(8, 6, 5, 4, 4, 4, 4, 3, 0), Array(6, 5, 4, 4, 4, 2, 1, 1, 0), Array(8, 6, 5, 4, 4, 3, 3, 2, 0)),
     new Character("Ox Bellows", 23, "October 18th", "Football, Shiny Objects", 3, 5, 5, 5, Array(6, 5, 5, 4, 3, 2, 2, 2, 0), Array(8, 8, 7, 6, 6, 5, 5, 4, 0), Array(7, 6, 5, 5, 4, 3, 2, 2, 0), Array(6, 6, 5, 5, 4, 4, 2, 2, 0)),
     new Character("Darrin 'Flash' Williams", 20, "June 6th", "Track, Music, Shakespearean Literature", 3, 5, 5, 5, Array(8, 7, 7, 6, 5, 4, 4, 4, 0), Array(7, 6, 6, 5, 4, 3, 3, 2, 0), Array(7, 5, 5, 5, 4, 3, 2, 1, 0), Array(7, 5, 5, 5, 4, 3, 3, 2, 0)),
